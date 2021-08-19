85f4f5f066716853c7e04e9663588b2b0f45de47
Andrii <shvaika.andrii@gmail.com>
2021-08-19 11:05:12 +0300

    uploaded final files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { interval } from "rxjs";
+import { map } from "rxjs/operators";
+
+import Timer from "./components/Timer/Timer";
+
+import "./App.css";
 
 function App() {
+  const [timer, setTimer] = useState("");
+  const [savedNum, setSavedNum] = useState(0);
+  const [subscription, setSubscription] = useState("");
+  const [prevent, setPrevent] = useState(true);
+
+  const onStartHandler = () => {
+    if (!subscription) {
+      const timerSubscription = interval(1000)
+        .pipe(map((v) => v + 1))
+        .subscribe((v) => {
+          setTimer(v + savedNum);
+        });
+      setSubscription(timerSubscription);
+    } else {
+      subscription.unsubscribe();
+      setTimer(0);
+      setSavedNum(0);
+      setSubscription("");
+    }
+  };
+
+  const onWaitHandler = () => {
+    if (prevent) {
+      setPrevent(false);
+      const timerInstance = setTimeout(function () {
+        setPrevent(true);
+        clearTimeout(timerInstance);
+      }, 300);
+    } else {
+      if (subscription) {
+        subscription.unsubscribe();
+      }
+      setSavedNum(timer);
+      setSubscription("");
+    }
+  };
+
+  const onResetHandler = () => {
+    if (subscription) {
+      subscription.unsubscribe();
+    }
+
+    const timerSubscription = interval(1000).subscribe((v) => {
+      setSavedNum(0);
+      setTimer(v);
+    });
+
+    setSubscription(timerSubscription);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Timer timePassed={timer ? timer : savedNum} />
+      <div className="App-buttons">
+        <button onClick={onStartHandler} className="start-button">
+          Start/Stop
+        </button>
+        <button onClick={onWaitHandler} className="wait-button">
+          Wait
+        </button>
+        <button onClick={onResetHandler} className="reset-button">
+          Reset
+        </button>
+      </div>
     </div>
   );
 }
